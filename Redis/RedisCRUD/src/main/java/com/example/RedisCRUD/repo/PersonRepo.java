f62997bacca60c4b36cd58150a8960ddef6d07ba
Anjali Kumari <sayhellotoanjali@gmail.com>
2024-08-12 14:50:32 +0530

    Delete Redis directory

--- Redis/RedisCRUD/src/main/java/com/example/RedisCRUD/repo/PersonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.RedisCRUD.repo;
-
-import com.example.RedisCRUD.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepo extends JpaRepository<Person, Integer> {
-    Person findByEmail(String email);
-
-}
